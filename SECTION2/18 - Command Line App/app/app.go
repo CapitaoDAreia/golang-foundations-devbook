1776523e7e6422a5fec9c6f470225878ce2afba9
Igor Silva <danieligord7@hotmail.com>
2023-01-16 20:56:31 -0300

    3-GOLANG | Mini command line application: searching for servers.

--- SECTION2/18 - Command Line App/app/app.go
+++ SECTION2/18 - Command Line App/app/app.go
@@ -15,18 +15,25 @@ func Generate() *CliPackage.App {
 
 	app.Name = "Command Line Application"
 	app.Usage = "Search for names and servers IP's."
+	flags := []CliPackage.Flag{
+		cli.StringFlag{
+			Name:  "host",
+			Value: "google.com.br",
+		},
+	}
 
 	app.Commands = []CliPackage.Command{
 		{
-			Name:  "ip",
-			Usage: "search for ips in web",
-			Flags: []CliPackage.Flag{
-				cli.StringFlag{
-					Name:  "host",
-					Value: "google.com.br",
-				},
-			},
+			Name:   "ip",
+			Usage:  "search for ips in web",
+			Flags:  flags,
 			Action: searchIps,
+		},
+		{
+			Name:   "servers",
+			Usage:  "Search for servers on web.",
+			Flags:  flags,
+			Action: searchForServers,
 		},
 	}
 
@@ -43,5 +50,18 @@ func searchIps(c *CliPackage.Context) {
 
 	for _, ip := range ips {
 		fmt.Printf("Public IP: %v\n", ip)
+	}
+}
+
+func searchForServers(c *CliPackage.Context) {
+	host := c.String("host")
+
+	servers, err := net.LookupNS(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, server := range servers {
+		fmt.Printf("Servers: %v\n", server)
 	}
 }
