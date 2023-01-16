7d35f50365bd6b1e800ec83941a005223cfa11a2
Igor Silva <danieligord7@hotmail.com>
2023-01-16 20:43:13 -0300

    3-GOLANG | Mini command line application: searching for public ip..

--- SECTION2/18 - Command Line App/app/app.go
+++ SECTION2/18 - Command Line App/app/app.go
@@ -1,13 +1,47 @@
 package app
 
-import CliPackage "github.com/urfave/cli"
+import (
+	"fmt"
+	"log"
+	"net"
 
-//This function will return the Command Line Application.
+	"github.com/urfave/cli"
+	CliPackage "github.com/urfave/cli"
+)
+
+// This function will return the Command Line Application.
 func Generate() *CliPackage.App {
 	app := CliPackage.NewApp()
 
 	app.Name = "Command Line Application"
 	app.Usage = "Search for names and servers IP's."
 
+	app.Commands = []CliPackage.Command{
+		{
+			Name:  "ip",
+			Usage: "search for ips in web",
+			Flags: []CliPackage.Flag{
+				cli.StringFlag{
+					Name:  "host",
+					Value: "google.com.br",
+				},
+			},
+			Action: searchIps,
+		},
+	}
+
 	return app
+}
+
+func searchIps(c *CliPackage.Context) {
+	host := c.String("host")
+
+	ips, err := net.LookupIP(host)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, ip := range ips {
+		fmt.Printf("Public IP: %v\n", ip)
+	}
 }
